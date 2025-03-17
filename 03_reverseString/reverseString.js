e63fe27ae2c28fce286586f84ede8ace91383aa4
LFG <198674398+L217-dev@users.noreply.github.com>
2025-03-16 19:31:11 -0500

    Solve 03 reverseString.js: -reverse the string using a for loop inverse

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let reverseString = ''
+    for (i=(string.length -1);i>=0;i--){
+        reverseString+=string[i]
+    }
+    return reverseString
 };
-
+console.log(reverseString('hello`'))
 // Do not edit below this line
 module.exports = reverseString;
