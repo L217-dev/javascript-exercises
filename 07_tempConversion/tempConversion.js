e3edadff7200060a0e6cb8ee6bd218ad5c042bee
LFG <198674398+L217-dev@users.noreply.github.com>
2025-03-16 19:44:48 -0500

    Solve 07 tempConversion.js: -Use toFixed(1) to round to one decimal >>Return a starting -Use Number to change to number type afer use toFixed()

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,14 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahTempeture) {
+  return Number(((fahTempeture-32)*5/9).toFixed(1))
+
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsiusTempeture) {
+  return Number(((celsiusTempeture*9/5) + 32).toFixed(1))
 };
-
+ convertToCelsius(32)
+ convertToFahrenheit(0)
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
