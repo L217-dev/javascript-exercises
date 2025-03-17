4279a4e648bfed6a60ddb8febb367a074ee506cf
LFG <198674398+L217-dev@users.noreply.github.com>
2025-03-16 19:27:37 -0500

    Solve 02 repeatString.js: -Create a variable to storage the repeatString -Return "ERROR" if the repeat number is not a number

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,13 @@
-const repeatString = function() {
-
-};
-
+const repeatString = function(string,num) {
+    let repearString = "";
+    if (num>=0){ 
+        for (i=0; i<num; i++) {
+                repearString+=string
+            }
+        return repearString;
+    }else{
+        return 'ERROR'
+    }
+}
 // Do not edit below this line
 module.exports = repeatString;
