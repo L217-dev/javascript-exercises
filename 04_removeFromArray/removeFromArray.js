dea572762af118bcb1dc64a7437b12f078694a89
LFG <198674398+L217-dev@users.noreply.github.com>
2025-03-16 19:33:31 -0500

    Solve 04 removeFromArray.js: -using arguments to allow more than one value to remove -using filter to remove from the array using a storage filterArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
 const removeFromArray = function() {
+    let filterArray = arguments[0]
+    if (arguments.length==2){
+        filterArray=filterArray.filter(value=> value!=+arguments[1] ) //take out the requested value to deleted 
+    }else{
+        for (const argument of arguments){
+            filterArray=filterArray.filter(value=> value!==argument) //evaluate and take out multiples value to deleted
+        }
+     }
+return filterArray
 };
 
 // Do not edit below this line
