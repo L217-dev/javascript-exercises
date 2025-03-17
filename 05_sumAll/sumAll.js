d43dc7483222d0065ab93a684c52501786efb096
LFG <198674398+L217-dev@users.noreply.github.com>
2025-03-16 19:36:24 -0500

    Solve 05 sumAll.js: -Create a New function to evaluete if is a integer number or number -Use if to evaluete if is a negative number -Use if is not number, negative or not integer return "ERROR" -Use if to change position if the firstValue is bigger than the secondValue

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function(startNumber,endNumber) {
+    function isNotInteger(startNumber,endNumber){
+        return (startNumber%1!==0 || endNumber%1!==0 || typeof(startNumber)!=='number'|| typeof(endNumber)!=='number')
+    }
+    if (startNumber<0 || endNumber<0 || isNotInteger(startNumber,endNumber)){
+        return "ERROR"
+    }
+    else if (startNumber>endNumber){
+        [startNumber,endNumber]=[endNumber,startNumber]
+    }
+   
+    let total = 0
+    let addedValue=startNumber
+    for (let step=0; step<=(endNumber-startNumber); step++){
+        total+=addedValue
+        addedValue++
+    }
+return total
 };
 
 // Do not edit below this line
